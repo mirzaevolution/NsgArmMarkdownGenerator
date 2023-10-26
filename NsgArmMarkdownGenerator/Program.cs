84c7986183c481ab3ec775058224a2940afb6d15
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2023-10-26 16:04:20 +0700

    Add project files.

---
+++ NsgArmMarkdownGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NsgArmMarkdownGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
